e11a1109a7e07ca6ee0bad55561552af94a141b2
Voste126 <steveaustine126@gmail.com>
2023-08-21 18:20:51 +0300

    my commit

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+         badge = f'Hello, my name is {name}.'
+         badges.append(badge)
+    return badges
+        
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    for index, name in enumerate(names, start=1):
+        guest_msg = f"Hello, {name}! You'll be assigned to room {index}!"
+        rooms.append(guest_msg)
+    return rooms
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+    for room in rooms:
+        print(room)
